5c23b3c18fa7bced1766b54b406c101d2fb33d4a
Hassen199 <jaffhassen@gmail.com>
2022-08-31 17:23:46 +0200

    first commit

---
+++ checkpoint-API-main/src/Components/UserList.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './UserList.css'
+
+const UserList = () => {
+    const [users, setUsers] = useState([]);
+    const [errMsg, setErrMsg] = useState('');
+
+    useEffect(() => {
+        axios.get('https://jsonplaceholder.typicode.com/users')
+
+        .then(response =>{console.log(response.data)
+            setUsers(response.data)
+        })
+
+        .catch(error => {
+            console.log(error)
+            setErrMsg(error)
+        })
+    }, [setUsers])
+
+    return (
+        <ul className='container'>
+            <h1 className='title'>List of users</h1>
+            {users.map(user => (
+            <div className='users'>
+                <p key={user.id}> Name : {user.name}</p>
+                <h5> UserName : {user.username}</h5>
+                <h5>email: {user.email}</h5>
+                <h6>phone: {user.phone}</h6>
+                <hr/>
+            </div>
+            ))}
+            {errMsg? <h4>{errMsg}</h4> : null} 
+        </ul>
+    )
+}
+
+export default UserList
